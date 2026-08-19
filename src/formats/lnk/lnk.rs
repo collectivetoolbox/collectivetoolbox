b65ef66aa21700ae971796957b5e7adb1a95be4a
Your Name <you@example.com>
2026-08-18 23:08:17 -0400

    docs: Clean up lnk and pan headers

--- src/formats/lnk/lnk.rs
+++ src/formats/lnk/lnk.rs
@@ -1,6 +1,27 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 // SPDX-License-Identifier for parts derived from lnk_rs: MIT
+/*
+This file is part of Collective Toolbox, a database and document workspace and utilities.
+Copyright (C) 2026 Collective Toolbox Developers
+Contact: info@collectivetoolbox.com
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Copyright (c) 2023 Lily Hopkins
 // From https://github.com/lilopkins/lnk-rs
+
+// See additional licensing details at end of file.
 
 #[expect(
     unused_imports,
