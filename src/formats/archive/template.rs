18103573975be642644c210759336204a713dc86
Your Name <you@example.com>
2026-09-06 17:55:24 -0400

    feat: Stub archive crate; add symlink for formats

---
+++ src/formats/archive/template.rs
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+This file is part of Collective Toolbox, a database and document workspace and utilities.
+Copyright (C) 2026 Collective Toolbox Developers
+Contact: info@collectivetoolbox.com
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+//! File archive formats - tar; ZIP; etc.
+
+#[expect(
+    unused_imports,
+    clippy::wildcard_imports,
+    reason = "Standard workspace crate prelude"
+)]
+pub(crate) use ctb_utilities::*;
+
+pub mod instruction_sets;
+
+static PAN_DATA_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/data");
+
+pub(crate) fn get_pan_data(key: &str) -> Option<Vec<u8>> {
+    get_embedded_asset(&PAN_DATA_DIR, key)
+}
+
+#[cfg(test)]
+#[allow(
+    clippy::panic,
+    clippy::expect_used,
+    clippy::unwrap_used,
+    clippy::unwrap_in_result,
+    clippy::panic_in_result_fn,
+    clippy::indexing_slicing,
+    clippy::arithmetic_side_effects,
+    reason = "Standard repository test boilerplate"
+)]
+mod tests {
+    use crate::utilities::{assert_vec_u8_eq, assert_vec_u8_ok_eq};
+    use ctb_formats_utilities::assert_string_ok_eq;
+
+    use super::*;
+
+}
